b0c4179c3edf86adcd1a0660d5d97283ab84164a
Дмитрий <dimadrumfire@gmail.com>
2022-12-20 19:57:50 +0600

    changes add !

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,5 @@
 import java.time.Year;
 
-class Book {
-
-    String bookName;
-    Author bookAuthor;
-    int bookYear;
-
-
-    public Book(String bookName, Author bookAuthor, int bookYear) {
-        this.bookName = bookName;
-        this.bookYear = bookYear;
-        this.bookAuthor = bookAuthor;
-
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public int getBookYear() {
-        return this.bookYear;
-    }
-
-    public void setBookYear(int bookYear) {
-        this.bookYear = bookYear;
-    }
-}
-
-class Author {
-    String authorFirstName;
-    String authorMiddleName;
-    String authorLastName;
-
-
-    public Author(String authorFirstName, String authorMiddleName, String authorLastName) {
-        this.authorFirstName = authorFirstName;
-        this.authorMiddleName = authorMiddleName;
-        this.authorLastName = authorLastName;
-
-    }
-
-
-    public String getAuthorFirstName() {
-        return this.authorFirstName;
-    }
-
-    public String getAuthorMiddleName() {
-        return this.authorMiddleName;
-    }
-
-    public String getAuthorLastName() {
-        return this.authorLastName;
-    }
-
-
-}
 
 
 public class Main {
@@ -66,9 +11,12 @@ public class Main {
         Author chehov = new Author("Антон", "Павлович", "Чехов");
         Book sadCherry = new Book("Вишневый сад", chehov, 1904);
         Book warAndPeace = new Book("Война и Мир", tolstoy, 1867);
-        System.out.println("sadCherry.bookYear = " + sadCherry.bookYear);
+        System.out.println("sadCherry.bookYear = " + sadCherry.getBookYear());
         sadCherry.setBookYear(1970);
-        System.out.println("sadCherry.bookYear = " + sadCherry.bookYear);
-        System.out.println("warAndPeace = " + tolstoy.authorMiddleName);
+        System.out.println("sadCherry.bookYear = " + sadCherry.getBookYear());
+        System.out.println("warAndPeace = " + warAndPeace.getBookName());
+        Book annaKarenina = new Book("Анна Каненина", tolstoy, 1877);
+        System.out.println("tolstoy.getAuthorMiddleName() = " + tolstoy.getAuthorMiddleName());
     }
+
 }
